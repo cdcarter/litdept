527334cb635fe7c03fcd4d6a500ebbf9599be241
Christian Carter <cdcarter@gmail.com>
2014-03-03 12:12:41 -0800

    starting somewhere

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Litdept::Application.config.secret_token = '6c5b8a76080a2caf86bf0f4b16a2128d6d6ed298655d4c5a07d733d286780bb6a21aa8d941d30b17ff4dff0a7837765ef9006b20c4ea8ffa9b74f162bb45f6c2'
