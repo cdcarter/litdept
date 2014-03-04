d9aecf97b27ee3e58d117a6dbb59ed962b48cb54
Christian Carter <cdcarter@gmail.com>
2014-03-03 17:59:09 -0800

    a super basic dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,29 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+
+    columns do
+      column do
+        panel "Must-Read Plays" do
+          scripts = Script.where(must_read: true)
+          if scripts.empty?
+            para 'No scripts are currently marked "must read."'
+          else
+            ul do
+              scripts.each do |script|
+                li link_to(script.title, script_path(script))
+              end
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Info" do
+          para "Welcome to the Seattle Repertory Theatre Literary Database. "
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
